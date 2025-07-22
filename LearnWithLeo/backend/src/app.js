8d741b56ba16f3ead188d3c9597d4f13ddf0cc0c
CodeWithCal-W <calmikew@gmail.com>
2025-07-22 21:53:44 +0300

    Added secure user authentication with validation and JWT

--- LearnWithLeo/backend/src/app.js
+++ LearnWithLeo/backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const productRoutes = require('./routes/productRoutes');
 const storyRoutes = require('./routes/storyRoutes');
 const lessonRoutes = require('./routes/lessonRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/stories', storyRoutes);
 app.use('/api/lessons', lessonRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
